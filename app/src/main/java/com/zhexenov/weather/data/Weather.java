1e9af2c4dd5c39545398543a53e8b8138d466ff1
Jex <fitsam01@gmail.com>
2018-07-04 23:10:35 +0600

    Weather model

--- app/src/main/java/com/zhexenov/weather/data/Weather.java
+++ app/src/main/java/com/zhexenov/weather/data/Weather.java
@@ -1,4 +1,16 @@
 package com.zhexenov.weather.data;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "weathers")
 public class Weather {
+
+    @PrimaryKey
+    @ColumnInfo(name = "cityid")
+    int id;
+
+
 }
